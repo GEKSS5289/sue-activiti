44f03fd5a3a4149fe2933b39d2ca52f9eaeb1dc9
GEKSS5289 <496966859@qq.com>
2020-09-13 09:19:48 +0800

    构建Activiti

---
+++ src/main/java/com/sue/sueactiviti7/SueActiviti7Application.java
@@ -0,0 +1,13 @@
+package com.sue.sueactiviti7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SueActiviti7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SueActiviti7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sue/sueactiviti7/SueActiviti7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sue.sueactiviti7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SueActiviti7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
