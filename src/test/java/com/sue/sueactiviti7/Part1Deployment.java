7bda2cd1a67f722a51aef4e6982fbd214e8c2858
GEKSS5289 <496966859@qq.com>
2020-09-13 11:09:02 +0800

    构建Activiti

---
+++ src/test/java/com/sue/sueactiviti7/Part1Deployment.java
@@ -0,0 +1,56 @@
+package com.sue.sueactiviti7;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.repository.Deployment;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.zip.ZipInputStream;
+
+/**
+ * @author sue
+ * @date 2020/9/13 10:14
+ */
+
+@SpringBootTest
+public class Part1Deployment {
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+    @Test
+    public void initDeploymentBPMN(){
+        String filename = "BPMN/Part1_Deployment.bpmn";
+        Deployment deployment = repositoryService.createDeployment()
+                .addClasspathResource(filename)
+                .name("SUE部署测试")
+                .deploy();
+        System.out.println(deployment.getName());
+    }
+
+    @Test
+    public void initDeploymentZIP(){
+        InputStream fileInputStream = this.getClass().getClassLoader()
+                .getResourceAsStream("BPMN/Part1_Deployment.zip");
+        ZipInputStream zip = new ZipInputStream(fileInputStream);
+        Deployment deployment = repositoryService.createDeployment()
+                .addZipInputStream(zip)
+                .name("流程部署ZIP")
+                .deploy();
+        System.out.println(deployment.getName());
+    }
+
+    @Test
+    public void getDeployments(){
+        List<Deployment> list = repositoryService.createDeploymentQuery().list();
+        for(Deployment dep:list){
+            System.out.println("id:"+dep.getId());
+            System.out.println("name"+dep.getName());
+            System.out.println("time"+dep.getDeploymentTime());
+            System.out.println("Key"+dep.getKey());
+        }
+    }
+}
---
+++ src/test/java/com/sue/sueactiviti7/Part2ProcessDefinition.java
@@ -0,0 +1,10 @@
+package com.sue.sueactiviti7;
+
+/**
+ * @author sue
+ * @date 2020/9/13 11:08
+ */
+
+
+public class Part2ProcessDefinition {
+}
